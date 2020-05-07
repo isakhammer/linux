c7e8893cf65e9fdec4260294faa42c99b3d051f7
Isak Hammer <isakhammer@gmail.com>
2020-05-07 12:27:05 +0200

    Added comment about sourcing in tmux

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -3,3 +3,7 @@
 #source-file ~/.linux_runtime/tmux/tmux_plugins.conf
 cp tmux.conf ~/.tmux.conf
 cp tmux.conf.local ~/.tmux.conf.local
+
+# type this in terminal in tmux to source new changes
+#$ tmux source ~/.tmux.conf
+
