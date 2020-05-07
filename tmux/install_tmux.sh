d937406d3fa84685014a323b43c608a30b8d0473
Isak Hammer <isakhammer@gmail.com>
2020-05-07 12:25:26 +0200

    Updated tmux conf and installation script

--- tmux/install_tmux.sh
+++ tmux/install_tmux.sh
@@ -1,3 +1,5 @@
 
-#Not tested
-source-file ~/.linux_runtime/tmux/tmux_plugins.conf
+#
+#source-file ~/.linux_runtime/tmux/tmux_plugins.conf
+cp tmux.conf ~/.tmux.conf
+cp tmux.conf.local ~/.tmux.conf.local
