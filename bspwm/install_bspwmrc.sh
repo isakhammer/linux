ab9f20b2e9cd57483d9bfd33012fe76e90f7921e
ISAK <ISAKHAMMER@GMAIL.COM>
2020-06-04 21:00:46 +0200

    added bspwm

---
+++ bspwm/install_bspwmrc.sh
@@ -0,0 +1,7 @@
+sudo apt install dmenu
+sudo apt intall bspwm
+
+mkdir -p ~/.config/bspwm
+mkdir -p ~/.config/sxhkd
+cp ~/.linux_runtime/bspwmrc ~/.config/bspwm/bspwmrc
+cp ~/.linux_runtime/sxhkdrc ~/.config/sxhkd/sxhkdrc
