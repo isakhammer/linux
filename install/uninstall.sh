e693ba1a7c6d4092bd18a5b5b4456cda60e7ce57
isak <isakhammer@gmail.com>
2020-07-09 10:49:27 +0200

    Removed uninstall script since it wont work

--- install/uninstall.sh
+++
@@ -1,9 +0,0 @@
-
-NOW=$(date +"%m-%d-%y-%T")
-echo "Making backup of your ~/.bashrc at: ~/.bashrc_backup_"$NOW
-cp ~/.bashrc ~/.bashrc_backup_$NOW
-sed -i "/.linux_runtime/d" ~/.bashrc
-source ~/.bashrc
-
-cd ~
-rm -Rf ~/.linux_runtime
