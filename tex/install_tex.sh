512c67fa590408665972860e86e93add3d8b540c
isak <isakhammer>
2020-07-22 23:57:02 +0200

    Fix template bug

--- tex/install_tex.sh
+++ tex/install_tex.sh
@@ -1,6 +1,9 @@
 sudo apt-get install inkscape
-sudo apt install python3-pip
-sudo pip3 install inkscape-figures
 
-sudo apt install latexmk
+sudo apt install python3-pip
+sudo pip3 install inkscape-figures # Inkscape plugin (sudo is important)
+
+sudo apt install latexmk #Compiler
 sudo apt-get install -y xdotool # for vimtex-zathura interface
+sudo apt install texlive-latex-extra # Latex packages
+
