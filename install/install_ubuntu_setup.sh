3d9c61faee071d1c56cfd55f70f84f3a2135d505
Isak Hammer <43385748+isakhammer@users.noreply.github.com>
2020-07-22 17:37:34 +0200

    Update install_ubuntu_setup.sh

--- install/install_ubuntu_setup.sh
+++ install/install_ubuntu_setup.sh
@@ -1,5 +1,5 @@
 
-bash ~/.linux_runtime/install/ubuntu/install_git_setup.sh
-bash ~/.linux_runtime/install/ubuntu/install_programs.sh
+bash ~/.linux_runtime/install/install_git_setup.sh
+bash ~/.linux_runtime/install/install_programs.sh
 bash ~/.linux_runtime/bashrc/install_bashrc.sh
 #bash ~/.linux_runtime/install/install_ros.sh
