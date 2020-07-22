985c7132ff701eeeddfd58ed0c043781c459b76b
isak <isakhammer>
2020-07-22 23:27:02 +0200

    Important to allow inkscape to access /bin/bash from root

--- tex/install_tex.sh
+++ tex/install_tex.sh
@@ -1,6 +1,6 @@
 sudo apt-get install inkscape
 sudo apt install python3-pip
-pip3 install inkscape-figures
+sudo pip3 install inkscape-figures
 
 sudo apt install latexmk
 sudo apt-get install -y xdotool # for vimtex-zathura interface
