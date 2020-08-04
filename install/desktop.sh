ce08ae41f18aaa594da94c44c3dd00765b1aa132
isak <isakhammer@gmail.com>
2020-08-04 23:27:12 +0200

    Added script to install regolith desktop

---
+++ install/desktop.sh
@@ -0,0 +1,3 @@
+# Source https://vitorbaptista.com/setting-up-ubuntu-20-04-with-regolith
+sudo add-apt-repository ppa:regolith-linux/release
+sudo apt install regolith-desktop i3xrocks-net-traffic i3xrocks-cpu-usage i3xrocks-time
