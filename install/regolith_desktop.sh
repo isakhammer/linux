5445e23f77f5c240f213c1b5f69b49c948664b00
isak <isakhammer@gmail.com>
2020-08-04 23:56:07 +0200

    Added default browser

--- install/regolith_desktop.sh
+++ install/regolith_desktop.sh
@@ -1,3 +1,6 @@
 # Source https://vitorbaptista.com/setting-up-ubuntu-20-04-with-regolith
 sudo add-apt-repository ppa:regolith-linux/release
 sudo apt install regolith-desktop i3xrocks-net-traffic i3xrocks-cpu-usage i3xrocks-time
+
+# Default browser to be chrome
+xdg-settings set default-web-browser chromium-browser.desktop
