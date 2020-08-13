55bece5f866033c56dbc5a60abe312005e9de4fd
isak <isakhammer@gmail.com>
2020-08-13 10:07:21 +0200

    Made installation script for docker.

--- docker/install_dockermachine.sh
+++
@@ -1,8 +0,0 @@
-# Introduction to dockermachine
-# https://www.macadamian.com/learn/docker-machine-basic-examples/
-
-# Installation:
-# https://stackoverflow.com/questions/47301434/sudo-docker-machine-command-not-found
-sudo apt install curl
-
-curl -L https://github.com/docker/machine/releases/download/v0.16.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine && chmod +x /tmp/docker-machine && sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
