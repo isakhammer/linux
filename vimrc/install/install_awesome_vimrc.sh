c1f3750d4ca521d946d68084f58dfddb3df110af
isak <isakhammer@gmail.com>
2020-09-29 11:13:56 +0200

    Added vimrc

---
+++ vimrc/install/install_awesome_vimrc.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+sudo apt install vim
+
+# Changing escape key and capslock.
+setxkbmap -option caps:swapescape
+
+touch ~/.vimrc
+
+cd ~/.vim_runtime
+NOW=$(date +"%m-%d-%y-%T")
+echo "Making backup of your ~/.vimrc at: ~/.vimrc_backup_"$NOW
+cp ~/.vimrc ~/.vimrc_backup_$NOW
+
+echo "Overwriting into your ~/.vimrc."
+echo "Starts writing"
+sleep 0.5
+echo "====="
+echo ""
+echo 'set runtimepath+=~/.vim_runtime
+
+source ~/.vim_runtime/vimrcs/basic.vim
+source ~/.vim_runtime/vimrcs/filetypes.vim
+source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
+
+try
+source ~/.vim_runtime/custom/my_vim_configs.vim
+catch
+endtry' |& tee ~/.vimrc
+
+echo ""
+echo "====="
+sleep 0.5
+echo "Finished writing"
